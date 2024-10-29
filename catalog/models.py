97fae00737c90f502758bf2271a5ec2d3e941379
Alexandr-lab-del <gordon675843@mail.ru>
2024-10-29 17:57:35 +0300

    fixes

--- catalog/models.py
+++ catalog/models.py
@@ -16,7 +16,7 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True)
-    image = models.ImageField(upload_to='products/photo/', blank=True, null=True)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
     category = models.ForeignKey('Category', related_name='products', on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
